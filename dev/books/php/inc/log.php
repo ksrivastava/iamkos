eae5e54eba050f8ac083805ec72d3815ad0038e1
Kaustubh Srivastava <kos.srivastava@gmail.com>
2013-12-05 21:32:19 -0500

    Dev folder untracked

--- dev/books/php/inc/log.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function logger($msg) {
-		$file = "logs/log";
-		$fh = fopen($file, "a") or die("can't open file");
-		$str = "[" . date("Y/m/d h:i:s", mktime()) . "] " . $msg;
-		fwrite($fh, $str . "\n");
-		fclose($fh);
-	}
-?>
